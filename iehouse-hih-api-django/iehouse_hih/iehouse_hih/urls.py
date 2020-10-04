475ecacdeb657c45f470231d4a099722fa589b38
Jhojan Gomez <jhojan.gomez4@gmailcom>
2020-10-04 15:17:27 -0500

    Agregados endpoints de acceso para el módulo de informes. ¡Crud de informe completado!

--- iehouse-hih-api-django/iehouse_hih/iehouse_hih/urls.py
+++ iehouse-hih-api-django/iehouse_hih/iehouse_hih/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('history-api/', include('history.urls')),
-    path('horary-api/', include('horary.urls'))
+    path('horary-api/', include('horary.urls')),
+    path('inform-api/', include('inform.urls'))
 ]
